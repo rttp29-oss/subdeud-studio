8fd6151b12ce37b0868c7845345bc241273717ad
ฟินส์ <rttp29@gmail.com>
2026-07-05 17:23:24 +0700

    fix import nextresponse

--- app/api/transcribe/route.ts
+++ app/api/transcribe/route.ts
@@ -1,5 +1,6 @@
-
-export const maxDuration = 60; // ขอขยายเวลาประมวลผลเป็น 60 วินาทีimport { NextResponse } from "next/server";
+export const maxDuration = 60;
+import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { GoogleAIFileManager } from "@google/generative-ai/server";
 import fs from "fs";
